0435b8ed056c5ab2812526f4bf4fcd588659832a
akhilkasuvojula <77549243+akhilkasuvojula@users.noreply.github.com>
2021-04-09 13:24:29 -0500

    committed

--- controllers/pet.js
+++ controllers/pet.js
@@ -13,8 +13,16 @@ exports.pet_list = async function(req, res) {
 res.send('NOT IMPLEMENTED: pet list');
 };
 // for a specific pet.
-exports.pet_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: pet detail: ' + req.params.id);
+exports.pet_detail = async function(req, res) {
+    console.log("detail"  + req.params.id)
+    try {
+        result = await pet.findById( req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+
 };
 // Handle pet create on POST.
 exports.pet_create_post = function(req, res) {
--- routes/pet.js
+++ routes/pet.js
@@ -3,4 +3,5 @@ const pet_controlers= require('../controllers/pet');
 var router = express.Router();
 /* GET pet */
 router.get('/', pet_controlers.pet_view_all_Page );
+router.get('/:id', pet_controlers.pet_detail);
 module.exports = router;
