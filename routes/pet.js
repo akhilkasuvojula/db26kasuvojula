a7bb1bc4b54b52a7a1432dbb0ab20bf3c2c06545
akhilkasuvojula <77549243+akhilkasuvojula@users.noreply.github.com>
2021-04-22 00:54:26 -0500

    part4

--- routes/pet.js
+++ routes/pet.js
@@ -1,6 +1,15 @@
 var express = require('express');
 const pet_controlers= require('../controllers/pet');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+// or redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
 /* GET pet */
 router.get('/', pet_controlers.pet_view_all_Page );
 
@@ -14,7 +23,7 @@ router.get('/detail', pet_controlers.pet_view_one_Page);
 router.get('/create', pet_controlers.pet_create_Page);
 
 /* GET create update page */
-router.get('/update', pet_controlers.pet_update_Page);
+router.get('/update',secured, pet_controlers.pet_update_Page);
 
 /* GET create costume page */
 router.get('/delete', pet_controlers.pet_delete_Page);
